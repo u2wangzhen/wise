e853519a2c1ea5943e595aefe563ca5f5a8199e9
u2wangzhen <308968013@qq.com>
2018-05-14 21:03:14 +0800

    列表和添加

--- wise/src/main/java/com/u2/wise/student/StudentController.java
+++ wise/src/main/java/com/u2/wise/student/StudentController.java
@@ -1,5 +1,9 @@
 package com.u2.wise.student;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.jfinal.kit.JsonKit;
 import com.jfinal.plugin.activerecord.Page;
 import com.u2.common.BaseController;
 import com.u2.wise.model.Student;
@@ -14,10 +18,27 @@ public class StudentController extends BaseController{
 		.set("name", "test")
 		.set("age", 2)发发
 		.set("sex",1).save();*/
-		System.out.println("ss");
-		Page<Student> page = Student.dao.paginate(1, 10,"select * ","from t_student ");
+		//System.out.println("ss");
+		
 		render(BASS_PATH+"student/list.html");
 		
 		
+	}
+	public void toAdd(){
+		
+		render(BASS_PATH+"student/add.html");
+	}
+	public void listData(){
+		
+		
+		int p = getParaToInt("page");
+		int limit = getParaToInt("limit");
+		Page<Student> page = Student.dao.paginate(p, limit,"select * ","from t_student ");
+		Map<String ,Object> map=new HashMap<String, Object>();
+		map.put("count", page.getTotalRow());
+		map.put("data", page.getList());
+		map.put("code", 0);
+		map.put("msg", "ok");
+		renderJson(map);
 	}
 }
