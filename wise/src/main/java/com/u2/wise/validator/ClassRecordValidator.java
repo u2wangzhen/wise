891c067fc78127307050369597aa436512a81327
u2wangzhen <308968013@qq.com>
2018-09-23 21:04:57 +0800

    优化课时修改

--- wise/src/main/java/com/u2/wise/validator/ClassRecordValidator.java
+++ wise/src/main/java/com/u2/wise/validator/ClassRecordValidator.java
@@ -10,6 +10,7 @@ import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.validate.Validator;
 import com.u2.common.ResultData;
+import com.u2.common.StringUtil;
 import com.u2.wise.server.ClassRecordService;
 import com.u2.wise.server.impl.ClassRecordServiceImpl;
 import com.xiaoleilu.hutool.date.DateUtil;
@@ -20,29 +21,29 @@ public class ClassRecordValidator extends Validator{
 	@Override
 	protected void validate(Controller c) {
 		// TODO Auto-generated method stub
-		String st=c.getPara("start_time");
-		String et=c.getPara("end_time");
-		String rd = c.getPara("classRecord.record_data");
-		String cid=c.getPara("classRecord.cid");
-		String start=rd+" "+st;
-		String end=rd+" "+et;
-		Date s=DateUtil.parseDateTime(start);
-		Date e=DateUtil.parseDateTime(end);
-		if(s.getTime()>=e.getTime()){
-			addError("msg", "开始时间晚于或者等于结束时间！");
-		}else{
-			Map<String, String> param=new HashMap<String, String>();
-			param.put("start1", start);
-			param.put("end1", end);
-			param.put("cid", cid);
-			List<Record> list = srv.list(param);
-			if(list!=null&&!list.isEmpty()){
-				addError("msg", "课时添加重复！");
+		String id=c.getPara("classRecord.id");
+		if(StringUtil.isEmpty(id)){
+			String st=c.getPara("start_time");
+			String et=c.getPara("end_time");
+			String rd = c.getPara("classRecord.record_data");
+			String cid=c.getPara("classRecord.cid");
+			String start=rd+" "+st;
+			String end=rd+" "+et;
+			Date s=DateUtil.parseDateTime(start);
+			Date e=DateUtil.parseDateTime(end);
+			if(s.getTime()>=e.getTime()){
+				addError("msg", "开始时间晚于或者等于结束时间！");
+			}else{
+				Map<String, String> param=new HashMap<String, String>();
+				param.put("start1", start);
+				param.put("end1", end);
+				param.put("cid", cid);
+				List<Record> list = srv.list(param);
+				if(list!=null&&!list.isEmpty()){
+					addError("msg", "课时添加重复！");
+				}
 			}
 		}
-		
-		
-		
 	}
 
 	@Override
